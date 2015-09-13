219237867b79c58439c7d4279454d43d19d43d92
Mateusz Łukomski <matrix.disc@gmail.com>
2015-09-13 16:10:54 +0200

    Refactored proto-tests in main

--- spatialHashMap/main.cpp
+++ spatialHashMap/main.cpp
@@ -1,17 +1,19 @@
 #include "SpatialHashMap.h"
+#include "GuidProivder.h"
 
 int main()
 {
 	SpatialHashMap m(10,2);
+	Guid guid;
 
 	auto result = m[3];
 
-	m.Add(1, -1.3f).Add(2, 3.5f).Add(3, 3.0f);
-	m.Delete(1);
+	auto object1 = guid.New();
+	auto object2 = guid.New();
+	auto object3 = guid.New();
+
+	m.Add(object1, -1.3f).Add(object2, 3.5f).Add(object3, 3.0f);
+	m.Delete(object1);
 	
-	
-
-
-
 	return 0;
 }
